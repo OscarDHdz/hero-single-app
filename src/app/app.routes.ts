fd9948119829e55264724b3d0bfedc295d4c40a4
Ozkar Hernández <oscardavid.hernandez.mx@gmail.com>
2018-06-17 00:12:29 -0500

    Added navigation by Btn and Link

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import {RouterModule, Routes} from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroComponent } from './components/hero/hero.component';
 
 const APP_ROUTES: Routes = [
   { path: 'home', component: HomeComponent},
   { path: 'heroes', component: HeroesComponent },
+  { path: 'hero/:id', component: HeroComponent },
   { path: 'about', component: AboutComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'home'}
 ];
