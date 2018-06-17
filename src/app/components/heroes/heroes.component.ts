4887b263a54f3885372f3d3195718eb8bbac5a1e
Ozkar Hernández <oscardavid.hernandez.mx@gmail.com>
2018-06-16 23:57:17 -0500

    Added Hero Service and Interface

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heroe  } from '../../services/heroes.service';
+import { log } from 'util';
 
 @Component({
   selector: 'app-heroes',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  heroes: Heroe[] = [];
+
+  constructor(private _heroesService: HeroesService) {}
 
   ngOnInit() {
+    this.heroes = this._heroesService.getHeroes();
+    console.log(this.heroes);
   }
 
 }
